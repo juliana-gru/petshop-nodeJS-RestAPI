e25866c85425264ca85113aa5441101ef538ba53
Juliana Grutzmann <juliana_grutzmann@hotmail.com>
2020-10-07 09:30:18 +1100

    add appointment table

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const customExpress = require('./config/custom-express');
+
 const connection = require('./infra/connection');
+const Tables = require('./infra/tables');
 
 connection.connect((error) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-    console.log('connected succesfully');
+    console.log('database connected');
+    Tables.init(connection);
 
     const app = customExpress();
 
     app.listen(3000, () => console.log('Server has started at port 3000'));
-
   }
 });
 
